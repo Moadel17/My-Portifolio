09f2187a4b1fc4f1a9d5aac8b07d6f76461ef24a
Moadel <moadel17@gmail.com>
2026-03-14 19:13:37 +0200

    add projects

--- src/component/3-project/dataComponent.jsx
+++ src/component/3-project/dataComponent.jsx
@@ -1,4 +1,5 @@
 import travelWeb from "../../Photos/travelweb.png";
+import coffeeBrandWeb from "../../Photos/Coffee Brand Web.png";
 
 export const buttons = [
   {
@@ -17,8 +18,8 @@ export const buttons = [
   },
   {
     key: 4,
-    title: "React",
-    category: "react",
+    title: "React JS",
+    category: "react js",
   },
 ];
 
@@ -32,5 +33,14 @@ export const cards = [
     link: "https://moadel17.github.io/Travel-Website/",
     more: "",
     category: "css",
+  },
+  {
+    key: 2,
+    img: coffeeBrandWeb,
+    title: "Coffee Brand",
+    description: "A brand coffee website that buy coffee, tea, juice and etc. ",
+    link: "https://moadel17.github.io/Coffee-Brand/",
+    more: "",
+    category: "react js",
   },
 ];
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import WindowWidth from "./context/windowWidth.jsx";
-import { BrowserRouter as Router } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <WindowWidth>
